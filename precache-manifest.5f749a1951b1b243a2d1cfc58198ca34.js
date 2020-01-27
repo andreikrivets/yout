e635495f208b94f1762d0db378b84883540d43c3
Andrei Krivets <etopushka@gmail.com>
2020-01-27 21:30:51 +0300

    Updates

---
+++ precache-manifest.5f749a1951b1b243a2d1cfc58198ca34.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3ab865bccbc36b119c3f0b5a7c40341",
+    "url": "/yout/index.html"
+  },
+  {
+    "revision": "26b9d9c2d335f344679b",
+    "url": "/yout/static/js/2.d6884bf8.chunk.js"
+  },
+  {
+    "revision": "aad0e403f8556317989a",
+    "url": "/yout/static/js/main.9d55e7b8.chunk.js"
+  },
+  {
+    "revision": "33e4c12840c4f4abf448",
+    "url": "/yout/static/js/runtime~main.dbb2fb62.js"
+  }
+]);
